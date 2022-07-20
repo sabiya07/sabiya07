b4f1660248dc9f07e75299c149b3cb9c96b7a9f5
sabiya07 <mohammedsabiya2@gmail.com>
2022-07-20 11:27:53 +0530

    Bitwise

---
+++ 2.js
@@ -0,0 +1,188 @@
+// assignment operator
+//var a=5,b=-3,c=-6
+
+//console.log(b,a,c, b+a-c)
+
+
+//console.log(c+','+b+','+a)
+//console.log(c<b<c)
+//console.log(c<b<a)
+//console.log(c>b<a)
+//console.log(a>b<c)
+//console.log(b<a<c)
+//console.log(c<c<c)
+
+
+
+//a=23,b=21
+//console.log(a+b)
+//console.log(a+6)
+ //console.log(9+b)
+ 
+ 
+//console.log(true<true>false<0)
+
+//var a=23,b=0,c=63
+//console.log(a-=b/c)
+//console.log(a>true<b)
+//var a=5,b=-3,c=-6
+//console.log(c!=a)
+//logical equality
+//console.log(c==b)
+//console.log(c==b)
+//console.log(c==b)
+//strict equality
+//console.log(true===1)// values+type
+
+
+
+
+
+//logical operators
+//TRUE-SUCCESS-PASS-ACTIVE
+//TRUE-1-VALUE
+
+//FALSE-FAIL-INACTIVE
+//FALSE-0-EMPTY
+
+//logical AND -&& (find first false)
+//A   B   R
+//f1  f2  f1
+//f   t   f
+//t   f   f
+//t1  t2  t2
+//console.log(3 && 4)
+//console.log(30 && 0)
+//console.log(0 && '')
+//console.log(false && 4)
+
+
+//logical OR - || -find first true
+//A   B   R
+//f1  f2  f2
+//f   t   t
+//t   f   t
+//t1  t2  t1
+//console.log('o.o' && ("") )
+
+//logical NOT-!
+//false -true
+//true-false
+
+//increment-post ,pre
+//pre increment
+//var a=4
+//a=a+1
+//++a
+//console.log(++a)
+//console.log(++a +a + ++a)
+//console.log(a + ++a + ++a)
+//console.log(a)
+
+//post increment
+//console.log(a++)
+//console.log(a)
+//console.log(a++ +a + a++)
+//console.log(a + a++ + a++)
+//console.log(a)
+
+
+
+//decrement-post,pre
+//var a=5
+//pre decrement
+//console.log(--a)
+//console.log(a)
+//console.log(--a +a + --a)
+//console.log(a + --a + --a)
+//console.log(a)
+
+//post decrement-prints and then updates
+//console.log(a--)
+//console.log(a)
+//console.log(a +a-- + a--)
+//console.log(a-- + a +a--)
+//console.log(a-- - a)
+//console.log(a)
+
+
+//var a=13, b=15
+//console.log(a--)
+//console.log(b--)
+//console.log(a + b-- + a-- - b--)
+//console.log(a-- +b +b-- + a--)
+//console.log(a-- - b)
+//console.log(a,b)
+//-----------------------------------------------
+
+//var a=20,b=22
+//console.log(a,b)
+//console.log(++a,++b,a++,b++)
+//console.log(++a + b-- + ++a-b++)
+//console.log(a-- + b + ++a +b-- + a--)
+//console.log(a-- - b+ a++ -b--)
+//console.log(a,b)
+
+//bitwise operators
+//bitwise AND -&
+//bitwise OR -|
+//bitwise NOT - ~
+//BITWISE XOR -^
+
+//bitwise AND -&
+// a  b   r
+//0   0   0
+//0   1   1
+//1   0   0
+//1   1   1
+
+//var a=40,b=25
+
+//console.log(a&b)
+
+
+//bitwise OR-|
+// a  b   r
+//0   0   0
+//0   1   1
+//1   0   1
+//1   1   1
+
+//a=33, b=27
+//console.log(a|b)
+
+//a=53,b=26
+//console.log(a|b)
+
+//bitwise xOR-^
+// a  b   r
+//0   0   0
+//0   1   1
+//1   0   1
+//1   1   0
+
+
+//var a=37v,b=2
+//left shift (<<)
+//console.log(a<<b)
+
+//right shift (>>)
+//a=117,b=3
+//console.log(a>>b)
+
+//bitwise NOT-~
+// ~N =-(N+1)
+//console.log(~2)
+
+
+//binary - 0bnnnn (2)
+//decimal = nnnnn (10)
+//octa; =0onnnn (8)
+//hexadecimal - 0xnnnn (16)
+
+//tostring
+//var a= 0b1000
+//console.log(a,tostring(10))//to convert to decimal
+//console.log(a,tostring(2))//to convert to binary
+//console.log(a,tostring(8))//to convert to octal
+//console.log(a,tostring(16))//to convert to hexadecimal
